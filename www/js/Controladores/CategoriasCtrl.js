bd01f25e5eb0610bfb999a6074e7781b43a60eec
Oscar Rodriguez <oscar.rodriguez@leadsius.com>
2016-06-21 17:49:39 -0400

    probando conn con server p/dispos.

--- www/js/Controladores/CategoriasCtrl.js
+++ www/js/Controladores/CategoriasCtrl.js
@@ -21,7 +21,7 @@ angular.module('starter')
     });
 
     $ionicPlatform.ready(function() {
-      $scope.pedido.uuid = ionic.Platform.device().uuid;
+      $scope.pedido.uuid = 12345678;//ionic.Platform.device().uuid;
     });
 
     push.register(function(token) {
@@ -30,18 +30,30 @@ angular.module('starter')
       push.saveToken(token);
     });
 
-    var dataDispositivo = {
+    var dataDispositivoCruda =  {
       'uuid' : $scope.pedido.uuid,
       'token': $scope.pedido.token
     };
 
-    // enviar uuid de dispositivo al servidor para ver si ya existe el dispositivo
+    var dataDispositivo = angular.fromJson(dataDispositivoCruda);
+
+    var defaultHTTPHeaders = {
+      'Content-Type':'aplication/json',
+      'Accept':'application/json'
+    };
+
+    $http.defaults.headers.post = defaultHTTPHeaders;
+
+    var tok = 123;
+    var uuid= 456;
+    console.log($scope.pedido.token);
     /**
      *
      * @type {string}
      */
-    var url = 'http://23.94.249.163/appDrinks/dispositivos/dispositivos.php';
-    $http.post(url,dataDispositivo , {headers: { 'Content-Type': 'application/json'}})
+    var urlDispositivo = 'http://23.94.249.163/appDrinks/dispositivos/dispositivos.php';
+    //$http.post(url,dataDispositivo , {headers: { 'Content-Type': 'application/json'}})
+    $http.post(urlDispositivo, {"token": tok,"uuid":uuid})
       .then(function (data){
         $scope.pedido.dispositivo = data.dispositivo; //todo poner dentro del pedido, el objeto Dispositivo (crear factory)
       });
@@ -69,48 +81,5 @@ angular.module('starter')
         $ionicLoading.hide();
       });
 
-    ////////////////
-    $scope.enviarPush = function(){
-      // Define relevant info
-      var jwt = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI4MjllZTIxOS01MzA4LTRhZDMtYWQ5NS1lZTQ3Y2YxMzhiMTMifQ.uNr-tPQzL63TOKShweE2Tychft3fPHF5H5Pc_8xmVeM';
-      var tokens = ['DEV-a62db0db-fd8c-45c1-bf06-4f823c5b4241'];
-      var profile = 'api-ionic-drinksApp';
-
-// Build the request object
-      var req = {
-        method: 'POST',
-        url: 'https://api.ionic.io/push/notifications',
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': 'Bearer ' + jwt
-        },
-        data: {
-          "tokens": tokens,
-          "profile": profile,
-          "notification": {
-            "title": "Hi",
-            "message": "Hello world!",
-            "android": {
-              "title": "Hey",
-              "message": "Hello Android!"
-            },
-            "ios": {
-              "title": "Howdy",
-              "message": "Hello iOS!"
-            }
-          }
-        }
-      };
-
-// Make the API call
-      $http(req).success(function(resp){
-        // Handle success
-        console.log("Ionic Push: Push success", resp);
-      }).error(function(error){
-        // Handle error
-        console.log("Ionic Push: Push error", error);
-      });
-    };
-    ///////////////
 
   });
