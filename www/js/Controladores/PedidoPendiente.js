26cf43858c5ce1cde71c39d3ee4a51ab17a38c34
Oscar Rodriguez <oscar.rodriguez@leadsius.com>
2016-07-06 13:23:47 -0400

    add pedidoRecibido()

--- www/js/Controladores/PedidoPendiente.js
+++ www/js/Controladores/PedidoPendiente.js
@@ -9,6 +9,9 @@ angular.module('starter')
   )
   {
 
+    /**
+     * Cancelar un pedido que esta como pendiente
+     */
     $scope.cancelarPedido = function(){
       var urlCambiarEstado = 'http://23.94.249.163/appDrinks/pedidos/cambiar_estado_pedido.php';
       $http.post(urlCambiarEstado, {idPedido: $rootScope.idUltPedido, estado:4}, {headers: { 'Content-Type': 'application/json'}})
@@ -17,6 +20,31 @@ angular.module('starter')
           $scope.pedido.limpiarTodo();
           var alertPopup = $ionicPopup.alert({
             title:   'Tu pedido fué cancelado',
+            buttons: [{
+              text: 'Aceptar',
+              type: 'button button-positive'
+            }]
+          });
+
+
+          alertPopup.then(function(res) {
+            $state.go('app.categorias');
+          });
+
+        });
+    };
+
+    /**
+     * Marcar como recibido un pedido
+     */
+    $scope.pedidoRecibido = function(){
+      var urlCambiarEstado = 'http://23.94.249.163/appDrinks/pedidos/cambiar_estado_pedido.php';
+      $http.post(urlCambiarEstado, {idPedido: $rootScope.idUltPedido, estado:3}, {headers: { 'Content-Type': 'application/json'}})
+        .then(function (data){
+          console.log(data);
+          $scope.pedido.limpiarTodo();
+          var alertPopup = $ionicPopup.alert({
+            title:   'Tu pedido se registró como Recibido',
             buttons: [{
               text: 'Aceptar',
               type: 'button button-positive'
